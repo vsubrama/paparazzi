90705b00e6a3944e902cdfce27c2fc562f0dfccd
dewagter <dewagter@gmail.com>
2014-10-21 10:13:48 +0200

    [module] dual battery measurement

---
+++ sw/airborne/modules/core/dual_battery.h
@@ -0,0 +1,13 @@
+#ifndef ADC_GENERIC_H
+#define ADC_GENERIC_H
+
+#include <inttypes.h>
+
+extern uint16_t bat1;
+extern uint16_t bat2;
+extern uint16_t adc_generic_val1;
+extern uint16_t adc_generic_val2;
+void adc_generic_init( void );
+void adc_generic_periodic( void );
+
+#endif /* ADC_GENERIC_H */
